f2be4da78d55ddaa44dd28e49cb8947bbc13ef9e
Burke Holland <burkeholland@gmail.com>
2020-04-10 17:29:52 -0500

    Add name and created fields to feed

--- src/Feed.js
+++ src/Feed.js
@@ -19,15 +19,17 @@ const Feed = () => {
   return (
     <div className="feed">
       <div id="bumper" className="line-item"></div>
-      {updates.map(({ name, video }) => (
+      {updates.map(({ name, video, created }) => (
         <div className="columns line-item is-vcentered">
           <div className="column is-narrow feed-line">
+            <span class="created">{created}</span>
             <div class="time-line"></div>
           </div>
           <div className="column feed-item">
             <div className="">
               <div className="box">
                 <div>
+                  <h3 className="is-size-4">{name}</h3>
                   <video controls width="450">
                     <source src={video} type="video/webm" />
                     Sorry, your browser doesn't support embedded videos.
