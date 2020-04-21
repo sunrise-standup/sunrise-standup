8c6569e40a1739ab0c28db892644d005534228aa
Burke Holland <burkeholland@gmail.com>
2020-04-21 16:19:54 -0500

    Remove detail text

--- src/Feed.js
+++ src/Feed.js
@@ -28,23 +28,10 @@ const Feed = () => {
             <div className="">
               <div className="box video-wrapper">
                 <h3 className="is-size-4">{name}</h3>
-                <div class="columns">
-                  <div className="column is-narrow">
-                    <video class="video" controls width="300px">
-                      <source src={video} type="video/webm" />
-                      Sorry, your browser doesn't support embedded videos.
-                    </video>
-                  </div>
-                  <div className="column">
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Nihil, commodi exercitationem expedita deleniti cum
-                      aliquam harum cupiditate voluptatum temporibus natus
-                      itaque nemo esse inventore ad iste officiis quas culpa
-                      maiores.
-                    </p>
-                  </div>
-                </div>
+                <video class="video" controls width="500px">
+                  <source src={video} type="video/webm" />
+                  Sorry, your browser doesn't support embedded videos.
+                </video>
               </div>
             </div>
           </div>
