686ceb3b4e74129f47164f0820f26a4c5d4cba2f
Brian Holt <btholt@gmail.com>
2020-04-22 10:58:41 -0700

    added allowlist to getsastokens

--- api/GetSASToken/index.js
+++ api/GetSASToken/index.js
@@ -1,4 +1,18 @@
 require("dotenv").config();
+const useNameFromQuerystring =
+  process.env.USE_NAME_FROM_QUERYSTRING &&
+  process.env.USE_NAME_FROM_QUERYSTRING.toLowerCase() === "true";
+// const acceptedUploaders = process.env.ACCEPTED_UPLOADERS
+//   ? process.env.ACCEPTED_UPLOADERS.split(",")
+//   : [];
+const acceptedUploaders = [
+  "btholt",
+  "burkeholland",
+  "fiveisprime",
+  "amandasilver",
+  "darquewarrior",
+  "abelsquidhead",
+];
 
 const {
   generateBlobSASQueryParameters,
@@ -7,7 +21,32 @@ const {
 } = require("@azure/storage-blob");
 
 module.exports = async function (context, req) {
-  const token = await generateSASToken(req.query.name);
+  const name = useNameFromQuerystring
+    ? req.params.name
+    : req.headers["X-MS-CLIENT-PRINCIPAL-NAME"];
+
+  if (!name) {
+    context.res = {
+      status: 401,
+      body: {
+        error: "user not authenticated",
+      },
+      headers: { "Content-Type": "application/json" },
+    };
+    return;
+  }
+  if (acceptedUploaders.length !== 0 && !acceptedUploaders.includes(name)) {
+    context.res = {
+      status: 403,
+      body: {
+        error: "user not authorized",
+      },
+      headers: { "Content-Type": "application/json" },
+    };
+    return;
+  }
+
+  const token = await generateSASToken(name);
   context.res = {
     body: { token },
     headers: { "Content-Type": "application/json" },
