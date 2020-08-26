221e9fa18d68920d12b9e1e17d2efd38382da224
timoleo23 <timoleo23@users.noreply.github.com>
2020-08-26 23:52:41 +0200

    Modify Tab config file

--- api/GetTabConfig/index.js
+++ api/GetTabConfig/index.js
@@ -1,12 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+const readFileAsync = util.promisify(fs.readFile);
+
 module.exports = async function (context, req) {
   // this function returns public keys for the app. Setting them here
   // does not make them secure because they can still be seen in the frontend.
   //
   // what it DOES do is make it so the entire app doesn't have to be redepoyed
   // if the keys are compromised.
-
-  context.res = {
-    // status: 200, /* Defaults to 200 */
-    body: { test:test },
+  try {
+    const html = await readFileAsync(path.resolve(__dirname, 'config.html'), 'utf8');
+    context.res = {
+      // status: 200, /* Defaults to 200 */
+        body: html,
+        headers: { "Content-Type" : "text/html; charset=utf-8" }
+    };
+  } catch (err) {
+    context.log.error('ERROR', err);
+    // This rethrown exception will be handled by the Functions Runtime and will only fail the individual invocation
+    throw err;
   };
 };
