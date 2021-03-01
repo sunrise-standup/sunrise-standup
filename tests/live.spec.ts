ef47e5a5129e61ea0f0c58f39dc6ffe5e5de4e5b
Arjun Attam <arjunattam@gmail.com>
2021-03-01 14:08:39 +0530

    Add tests for go-live

--- tests/live.spec.ts
+++ tests/live.spec.ts
@@ -1,7 +1,20 @@
-import { it, expect } from "@playwright/test";
+import { it, expect } from "./fixtures";
 
-it("loads the app", async ({ page }) => {
+const acsFrameUrl = /.*gentle-pond-06daa3a1e.azurestaticapps.net.*/;
+
+it("loads the app", async ({ page, isMobile }) => {
   await page.goto(process.env.APP_URL);
   const title = await page.title();
   expect(title).toBe("Sunrise Standup");
+});
+
+it("clicks on go live button", async ({ page, isMobile }) => {
+  await page.goto(process.env.APP_URL);
+  await Promise.all([
+    page.waitForResponse(acsFrameUrl),
+    page.click('"Go Live"')
+  ]);
+  await page.waitForSelector('.frame');
+  const acsFrame = page.frame({ url: /.*gentle-pond-06daa3a1e.azurestaticapps.net.*/ });
+  await acsFrame.waitForSelector('input[placeholder="Enter your name"]');
 });
