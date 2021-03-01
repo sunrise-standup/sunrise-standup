1440c2ca15b97fbb01603c4d76430b2a0bce158c
Burke Holland <burkeholland@gmail.com>
2021-03-01 22:23:22 +0000

    fix button issue on safari

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -26,7 +26,7 @@ const Nav = ({ user }) => {
           className="button navbar-burger burger"
           aria-label="menu"
           aria-expanded="false"
-          onClick={() => setMenuActive(menuActive)}
+          onClick={() => setMenuActive(!menuActive)}
         >
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
