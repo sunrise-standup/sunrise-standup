d182f53cd886b82df681f3869aae7e949bf54ed3
Burke Holland <burkeholland@gmail.com>
2021-03-01 20:39:46 +0000

    only show button when logged in

--- src/components/LiveButton.js
+++ src/components/LiveButton.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./LiveButton.css";
 
 const LiveButton = ({ user }) => {
+  const userisLoggedIn = user && user.isLoggedIn;
   const [isActive, setIsActive] = useState(false);
 
   function goLive() {
@@ -12,36 +13,39 @@ const LiveButton = ({ user }) => {
     setIsActive(false);
   }
 
-  return (
-    <span>
-      <button
-        className="button is-primary is-medium navbutton"
-        onClick={goLive}
-      >
-        Go Live
-      </button>
-      <div className={"modal " + (isActive ? "is-active" : "")}>
-        <div className="modal-background"></div>
-        <div className="modal-content">
-          <div class="frame">
-            <iframe
-              title="ACS"
-              allow="camera;microphone"
-              class="frame"
-              width="100%"
-              height="100%"
-              src="https://gentle-pond-06daa3a1e.azurestaticapps.net/?groupId=b4146fc0-77bb-11eb-a2f3-af18900971d5"
-            ></iframe>
-          </div>
-        </div>
+  if (userisLoggedIn) {
+    return (
+      <span>
         <button
-          className="modal-close is-large"
-          onClick={exitLive}
-          aria-label="close"
-        ></button>
-      </div>
-    </span>
-  );
+          className="button is-primary is-medium navbutton"
+          onClick={goLive}
+        >
+          Go Live
+        </button>
+        <div className={"modal " + (isActive ? "is-active" : "")}>
+          <div className="modal-background"></div>
+          <div className="modal-content">
+            <div class="frame">
+              <iframe
+                title="ACS"
+                allow="camera;microphone"
+                class="frame"
+                width="100%"
+                height="100%"
+                src="https://gentle-pond-06daa3a1e.azurestaticapps.net/?groupId=b4146fc0-77bb-11eb-a2f3-af18900971d5"
+              ></iframe>
+            </div>
+          </div>
+          <button
+            className="modal-close is-large"
+            onClick={exitLive}
+            aria-label="close"
+          ></button>
+        </div>
+      </span>
+    );
+  }
+  else return null;
 };
 
 export default LiveButton;
