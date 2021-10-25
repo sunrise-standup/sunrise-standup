3ca7c9b3047d7012b60c3f334897c8646c75930e
Marcus Felling <MarcusFelling@microsoft.com>
2021-10-25 17:25:42 -0500

    GitHub Actions setup

--- e2e/map.spec.ts
+++ e2e/map.spec.ts
@@ -3,10 +3,10 @@ const { test, expect } = require('@playwright/test');
 test('map', async ({ page }) => {
 
   // Go to https://www.sunrisestandup.com/
-  await page.goto('https://www.sunrisestandup.com/');
+  await page.goto('http://localhost:3000/');
 
   // Click text=Map
   await page.click('text=Map');
-  await expect(page).toHaveURL('https://www.sunrisestandup.com/map');
+  await expect(page).toHaveURL('http://localhost:3000/map');
 
 });
